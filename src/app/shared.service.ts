045723f78d55fa1b8bc531e3a952496b7bcce146
yhong <hyp0126@gmail.com>
2021-01-22 02:29:46 -0500

    add canvasjs.min.js (line chart lib) add DotEnv.ts (ignore list)

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, VirtualTimeScheduler } from 'rxjs';
+import * as DotEnv from './DotEnv';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  readonly APIUrl="http://localhost:8080/api";
-
   constructor(private http:HttpClient) { }
 
   getRoomData() {
-    return this.http.post(this.APIUrl+'/roomData',{token: sessionStorage.getItem('token')});
+    return this.http.post(DotEnv.API_URL+'/roomData',{token: sessionStorage.getItem('token')});
   }
 
   getTempData(date:Date):Observable<any[]> {
@@ -25,22 +24,22 @@ export class SharedService {
     var endTimeD = new Date(year, month, day, 23, 59, 59);
     var endTime = endTimeD.toUTCString();
 
-    return this.http.post<any[]>(this.APIUrl+'/temperature',{startTime, endTime, token: sessionStorage.getItem('token')});
+    return this.http.post<any[]>(DotEnv.API_URL+'/temperature',{startTime, endTime, token: sessionStorage.getItem('token')});
   }
 
   toggleLed(id:string) {
-    return this.http.post(this.APIUrl+'/led',{id, token: sessionStorage.getItem('token')})
+    return this.http.post(DotEnv.API_URL+'/led',{id, token: sessionStorage.getItem('token')})
       .subscribe((data:any) => {
       });
   }
 
   login(username:string, password:string) {
-    return this.http.post(this.APIUrl+'/login',{username, password})
+    return this.http.post(DotEnv.API_URL+'/login',{username, password})
   }
 
   logout() {
     sessionStorage.clear();
-    return this.http.post(this.APIUrl+'/logout',{token: sessionStorage.getItem('token')})
+    return this.http.post(DotEnv.API_URL+'/logout',{token: sessionStorage.getItem('token')})
       .subscribe((data:any) => {
         sessionStorage.clear();
       });
